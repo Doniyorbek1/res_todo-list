092b4657742adf6f740317f34f27fa06c301266d
Doniyorbek <99865105+Doniyorbek1@users.noreply.github.com>
2023-04-06 11:34:44 +0500

    add rest_framework and url api

--- api/views.py
+++ api/views.py
@@ -2,5 +2,9 @@ from .models import Todo
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
+class TodoView(APIView):
+    def get(self, request:Request)->Response:
+        pass
