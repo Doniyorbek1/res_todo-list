d786fd08cb91be12296c96867bfbad1c332f6dc6
Doniyorbek <99865105+Doniyorbek1@users.noreply.github.com>
2023-04-06 11:15:32 +0500

    serializer and models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length=20)
+    description = models.TextField(max_length=200)
+    completed = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.task
--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from .models import Todo
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
