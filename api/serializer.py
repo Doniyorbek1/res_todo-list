910fcb80579cc80092a3370a1ce6b03738e0a743
Doniyorbek <99865105+Doniyorbek1@users.noreply.github.com>
2023-04-11 09:21:11 +0500

    modelserializer

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Todo
 
-class TodoSerializer(serializers.Serializer):
-    task = serializers.CharField()
-    description = serializers.CharField()
-    
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+        
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "task": instance.task,
+            "update_at": instance.update_at,
+        }
